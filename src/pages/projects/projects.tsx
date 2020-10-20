fa0f9f1cb8130c5869e59ffbe7e9804bcabe0db3
cristobalszk <crisszkutnik@gmail.com>
2020-10-20 15:12:02 -0300

    Added projects squares and animations

--- src/pages/projects/projects.tsx
+++ src/pages/projects/projects.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import Square from "./square/square"
+import Square from "./square/square";
+import BG1 from "../../assets/img1.png";
 import "./projects.scss";
 
 export default function() {
 	return (
 		<div id="projects">
-			<Square />
-			<Square />
+			<Square bg={BG1} title="FMC Weekly"/>
+			<Square title="Sorting Visualizer"/>
 		</div>
 	);
 }
--- src/pages/projects/square/square.tsx
+++ src/pages/projects/square/square.tsx
@@ -1,27 +1,36 @@
-import React, { useState } from "react";
+import React, { useState } from "react"
+import Github from "../../../assets/github"
 import "./square.scss"
 
-const Square = (props) => {
-	const [mouseEnter, setMouse] = useState(false);
+const Square = props => {
+  const [mouseEnter, setMouse] = useState(false)
+  const [timeout, sTime] = useState()
 
-	const toggleMouse = () => {
-		setTimeout(() => {
-			setMouse(!mouseEnter)
-		}, 250);
-	}
+  let bg =
+    "linear-gradient(120.26deg, rgba(233, 69, 96, 0.9) 39.71%, rgba(255, 107, 0, 0.9) 105.48%)"
+  bg += props.bg ? `, url(${props.bg})` : ""
 
-	return (
-		<div onMouseLeave={toggleMouse} onMouseEnter={toggleMouse} className="project-square">
-			<div className="square-container">
-				<div className="square-title" style={{ display: `${mouseEnter ? "none" : "block"}` }}>
-					<h1>Sample text</h1>
-				</div>
-				<div className="square-data">
-
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div
+      className="project-square"
+      style={{ background: bg, backgroundSize: "cover" }}
+    >
+      <div className="square-title">
+        <h1>{props.title}</h1>
+      </div>
+      <div className="square-data">
+        <p>
+          Fewest Moves is a website developed for people who want to practice
+          the FMC event. It hosts a weekly competition where 3 scrambles are
+          given to the participants and they have one hour to find the shortest
+          solution they can.
+        </p>
+        <div>
+          <Github />
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default Square;
+export default Square
