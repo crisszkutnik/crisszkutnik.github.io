4a801d6376a6d8e2f15760ec6fffc802657d94e1
cristobalszk <crisszkutnik@gmail.com>
2020-10-21 14:38:43 -0300

    Added projects title

--- src/pages/projects/projects.tsx
+++ src/pages/projects/projects.tsx
@@ -11,6 +11,10 @@ let text1 = `Fewest Moves is a website developed for people who want to practice
 export default function () {
   return (
     <div id="projects">
+      <div id="projects-title">
+        <h1>MY PROJECTS</h1>
+        <hr />
+      </div>
       <Square
         bg={BG1}
         title="FMC Weekly"
