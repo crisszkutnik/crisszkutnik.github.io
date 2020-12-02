7a636120119fef3248616639388b6653d3ccc343
Cristobal Szkutnik <crisszkutnik@gmail.com>
2020-12-02 02:04:31 -0300

    {

--- src/pages/projects/projects.tsx
+++ src/pages/projects/projects.tsx
@@ -6,7 +6,11 @@ import "./projects.scss"
 let text1 = `Fewest Moves is a website developed for people who want to practice
           the FMC event. It hosts a weekly competition where 3 scrambles are
           given to the participants and they have one hour to find the shortest
-          solution they can.`
+          solution they can.`;
+
+let text2 = "An old page I made that shows how different sorting algorithms work. This is my first project ever made and definitely needs an update.";
+
+let text3 = "My portfolio. Yes, it is the page you are looking at. There's not much to tell.";
 
 export default function () {
   return (
@@ -18,13 +22,23 @@ export default function () {
       <Square
         bg={BG1}
         title="FMC Weekly"
-		  text={text1}
-		  tags={["ReactJS", "Node.js", "MongoDB", "Express.js"]}
-		  gh_link="https://github.com/crisszkutnik/fewestmoves-client"
-		  link="https://fmc-weekly.cubing.net"
+        text={text1}
+        tags={["ReactJS", "Node.js", "MongoDB", "Express.js"]}
+        gh_link="https://github.com/crisszkutnik/fewestmoves-client"
+        link="https://fmc-weekly.cubing.net"
       />
-      <Square title="Sorting Visualizer" />
-      <Square />
+      <Square
+        title="Sorting Visualizer"
+        text={text2}
+        tags={["HTML", "CSS", "JavaScript"]}
+        gh_link="https://github.com/crisszkutnik/sortingvisualizer"
+        link="https://crisszkutnik.github.io/sortingvisualizer/"
+      />
+      <Square
+        title="Portfolio"
+        text={text3}
+        tags={["GatsbyJS", "TypeScript", "SCSS"]}
+        gh_link="https://github.com/crisszkutnik/crisszkutnik.github.io" />
     </div>
   )
 }
